51525fa21c84e51ccc6f3e17309e934910d1509a
Saurav Kumal <sk.sauravkumal@gmail.com>
2020-05-05 21:50:08 +0545

    working code

--- Demo.py
+++ Demo.py
@@ -1,6 +1,8 @@
 import sim
 import sys
 import cv2
+import numpy as np
+import time
 #################################################################################### start conection to simulator
 sim.simxFinish(-1) # just in case, close all opened connections
 clientID = sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Get the client ID
@@ -13,12 +15,16 @@ else:
 ##################################################################################### get reference to objects in simulator
 errorCode, cameraHandle = sim.simxGetObjectHandle(clientID, 'Vision_sensor', sim.simx_opmode_oneshot_wait) # for camera
 sim.simxGetVisionSensorImage(clientID, cameraHandle, 0, sim.simx_opmode_streaming) # start image streaming from camera sensor
-
-returnCode = 1
-while returnCode !=0: 
-    returnCode, resolution, image = sim.simxGetVisionSensorImage(clientID, cameraHandle, 0, sim.simx_opmode_buffer)
-    print(returnCode)
-    
+  
 while True: 
     returnCode, resolution, image = sim.simxGetVisionSensorImage(clientID, cameraHandle, 0, sim.simx_opmode_buffer)
-    cv2.imshow('test',image)
+    if returnCode == sim.simx_return_ok:
+        img = np.array(image,dtype=np.uint8)
+        img.resize([resolution[1],resolution[0],3])
+        transformedImage = cv2.flip(img,0)
+        transformedImage = cv2.cvtColor(transformedImage,cv2.COLOR_BGR2RGB)
+        cv2.imshow('image',transformedImage)
+        cv2.waitKey(1)
+    elif returnCode == sim.simx_return_novalue_flag:
+        print ('no image yet')
+        pass    
